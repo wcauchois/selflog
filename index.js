61a02e406defd5dea33801ff365699cbb5b806af
Bill Cauchois <wcauchois@gmail.com>
2016-07-16 13:55:33 -0400

    commit

--- index.js
+++ index.js
@@ -9,25 +9,46 @@ const Schema = mongoose.Schema;
 import path from 'path';
 const bcrypt = Promise.promisifyAll(require('bcrypt-nodejs'));
 import session from 'express-session';
+import _ from 'lodash';
 import passport from 'passport';
+import csurf from 'csurf';
 import {Strategy as LocalStrategy} from 'passport-local';
 import {Strategy as TwitterStrategy} from 'passport-twitter';
+import {Strategy as SoundCloudStrategy} from 'passport-soundcloud';
+import swig from 'swig';
+import Twitter from 'twitter';
 const MongoStore = require('connect-mongo')(session);
 
-if (!process.env.PORT) {
+const config = _.extend({}, process.env);
+
+if (!config.PORT) {
   console.error("At least a PORT is required to use this program");
   process.exit(1);
 }
 
 const app = express();
 app.use(morgan('common'));
-app.use(express.static('dist'));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
-mongoose.connect(process.env.MONGODB_URL);
+const csrfProtection = csurf();
+
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+const isProduction = config.NODE_ENV === 'production';
+
+swig.setDefaults({cache: isProduction});
+
+if (!isProduction) {
+  app.disable('etag');
+}
+
+mongoose.connect(config.MONGODB_URL);
 
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: config.SESSION_SECRET,
   store: new MongoStore({mongooseConnection: mongoose.connection}),
   resave: false,
   saveUninitialized: false
@@ -44,9 +65,9 @@ passport.deserializeUser((id, done) => {
   User.findByIdAsync(id).asCallback(done);
 });
 
-passport.use(new LocalStrategy(
-  (name, password, done) => {
-    User.findOneAsync({name: name})
+passport.use('local', new LocalStrategy(
+  (username, password, done) => {
+    User.findOneAsync({username: username})
       .then(user => {
         if (!user) {
           return [false, {message: "Invalid username or password."}];
@@ -65,9 +86,51 @@ passport.use(new LocalStrategy(
   }
 ));
 
-if (process.env.NODE_ENV !== 'production') {
-  app.set('etag', false);
+passport.use('twitter', new TwitterStrategy({
+    consumerKey: config.TWITTER_CONSUMER_KEY,
+    consumerSecret: config.TWITTER_CONSUMER_SECRET,
+    callbackURL: config.SITE_ROOT + '/connect/twitter/callback'
+  },
+  function(token, tokenSecret, profile, done) {
+    const account = {
+      domain: 'twitter',
+      userId: profile.id,
+      token: token,
+      tokenSecret: tokenSecret,
+      profile: profile
+    };
+    done(null, account);
+  }
+));
+
+function registerConnectors(app, strategyName) {
+  app.get('/connect/' + strategyName, passport.authorize(strategyName));
+
+  app.get('/connect/' + strategyName + '/callback', passport.authorize(strategyName), (req, res) => {
+    req.user.addAccount(req.account);
+    req.user.saveAsync().then(() => res.redirect('/'));
+  });
 }
+
+passport.use('soundcloud', new SoundCloudStrategy({
+    clientID: config.SOUNDCLOUD_CLIENT_ID,
+    clientSecret: config.SOUNDCLOUD_CLIENT_SECRET,
+    callbackURL: config.SITE_ROOT + '/connect/soundcloud/callback'
+  },
+  function(token, tokenSecret, profile, done) {
+    const account = {
+      domain: 'soundcloud',
+      userId: profile.id,
+      token: token,
+      tokenSecret: tokenSecret,
+      profile: profile
+    };
+    done(null, account);
+  }
+));
+
+registerConnectors(app, 'twitter');
+registerConnectors(app, 'soundcloud');
 
 function apiEndpoint(handlerP) {
   return (req, res) => {
@@ -80,21 +143,28 @@ function apiEndpoint(handlerP) {
   };
 }
 
-app.get('/api/session', apiEndpoint(req => {
+app.get('/login', csrfProtection, (req, res) => {
+  res.render('login', {csrfToken: req.csrfToken()});
+});
+
+app.post('/login', csrfProtection, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login?failed=yes'
 }));
 
-app.post('/api/signup', apiEndpoint(req => {
-  var name = req.body.name;
-  var password = req.body.password;
+// XXX needs to be a reagular post handler
+app.post('/signup', apiEndpoint(req => {
+  const username = req.body.username;
+  const password = req.body.password;
   return Promise
     .try(() => {
-      if (!name) {
-        throw new Error("Name is required!");
+      if (!username) {
+        throw new Error("Username is required!");
       } else if (!password) {
         throw new Error("Password is required!");
       }
     })
-    .then(() => User.findOneAsync({name: name}))
+    .then(() => User.findOneAsync({username: username}))
     .then(maybeUser => {
       if (maybeUser) {
         throw new Error("A user with that name already exists.");
@@ -103,7 +173,7 @@ app.post('/api/signup', apiEndpoint(req => {
     })
     .then(passwordHash => {
       return User.createAsync({
-        name: name,
+        username: username,
         passwordHash: passwordHash
       });
     }).then(user => {
@@ -117,24 +187,66 @@ app.post('/api/signup', apiEndpoint(req => {
     });
 }));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+app.get('/', (req, res) => {
+  let twitterFeedP = Promise.resolve([]);
+  if (req.user) {
+    const apiClients = req.user.apiClients();
+    if (apiClients.twitter) {
+      twitterFeedP = apiClients.twitter
+        .getAsync('favorites/list');
+    }
+  }
+  twitterFeedP.then(twitterFeed => {
+    res.render('index', {
+      twitterFeed: twitterFeed
+    });
+  });
+});
+
+const accountSchema = Schema({
+  domain: String,
+  userId: String,
+  token: String,
+  tokenSecret: String,
+  profile: Object
 });
 
 const userSchema = Schema({
-  name: String,
-  passwordHash: String
+  username: String,
+  passwordHash: String,
+  accounts: [accountSchema]
 });
 
 userSchema.methods.render = function() {
   return {
-    name: this.name
+    username: this.username
   };
 };
 
-var User = mongoose.model('User', userSchema);
+userSchema.methods.addAccount = function(newAccount) {
+  this.accounts = [newAccount].concat(this.accounts.filter(a => a.domain !== newAccount.domain));
+};
 
-const port = process.env.PORT;
+userSchema.methods.apiClients = function() {
+  const apiClients = {};
+  if (this.accounts) {
+    this.accounts.forEach(account => {
+      if (account.domain === 'twitter') {
+        apiClients.twitter = Promise.promisifyAll(new Twitter({
+          consumer_key: config.TWITTER_CONSUMER_KEY,
+          consumer_secret: config.TWITTER_CONSUMER_SECRET,
+          access_token_key: account.token,
+          access_token_secret: account.tokenSecret
+        }));
+      }
+    });
+  }
+  return apiClients;
+}
+
+const User = mongoose.model('User', userSchema);
+
+const port = config.PORT;
 app.listen(port, () => {
   util.log(`Listening on port ${port}`);
 });
